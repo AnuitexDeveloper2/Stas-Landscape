67e87e031a98bbaf1eb99451f69e7cb47d9c482c
Volodymyr Honcharuk <vhoncharuk@amcbridge.com>
2023-06-30 13:30:34 +0300

    try create fixed sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "./index.scss";
 
 const Sidebar: React.FC = () => {
-  return <></>;
+  useEffect(() => {
+    const element = document.getElementById("sidebar");
+    if (element) {
+      const observer = new IntersectionObserver(
+        function (entries: any) {
+          if (entries[0].isIntersecting === true) {
+            element.classList.add("animation");
+          }
+        },
+        { threshold: [0] }
+      );
+      observer.observe(element);
+    }
+  }, []);
+  return (
+    <div className="sidebar-container" id="sidebar">
+      Hello
+    </div>
+  );
 };
 
 export default Sidebar;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,6 +4,7 @@ import HomeHeader from "./homeHeader/HomeHeader";
 import logoBg from "../../assets/images/logo-bg.svg";
 import TextAndImage from "../../components/textAndImage/TextAndImage";
 import { getTextAndImageItems } from "./home.helper";
+import Sidebar from "../../components/sidebar/Sidebar";
 
 const HomePage: React.FC = () => {
   return (
@@ -13,6 +14,7 @@ const HomePage: React.FC = () => {
         <div className="logo-bg">
           <img src={logoBg} alt="logo" />
         </div>
+        <Sidebar />
         <div className="home-body-title">
           <h2>Ландшафтный дизайн — простые решения от GardenPark</h2>
         </div>
